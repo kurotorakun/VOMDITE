6d77894fc53da7cc070b8ed7e07ddad0c64ad47a
kurotorakun <kurotora.kun@gmail.com>
2021-12-02 16:07:37 +0100

    Forcing esxi provider to use version 1.8.3
    With esxi provider last version 1.9.1 portgroups are created without inheriting the security policies of the vSwitches.

--- terraform-files/versions.tf
+++ terraform-files/versions.tf
@@ -4,6 +4,7 @@ terraform {
     # enable ESXi provider
     esxi = {
       source = "registry.terraform.io/josenk/esxi"
+      version = "1.8.3" # Current version (Dec/2021) is 1.9.1 but has some issue with Portgroup policy that impact on Lab
       #
       # For more information, see the provider source documentation:
       #
